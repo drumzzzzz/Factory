edf21bd3de5f4885c945210cd81c6f0024b204fb
nathaniel kennis <drummer_39@att.net>
2020-10-09 16:33:52 -0500

    Working!

--- Factory/main.cpp
+++ Factory/main.cpp
@@ -1,82 +1,189 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <sstream> 
 
 using namespace std;
 
-class Apple {
+class Subaru
+{
 public:
+    // Static factory method
+    static Subaru* make_subaru(const char* model);
 
-    // Factory Method, note it's static
-    static Apple* make_apple(string choice);
+    // Methods used polymorphically per model
 
-    // method that we will use polymorphically per product
-    virtual void io() = 0;
+    virtual void Model() = 0;
+	
+    virtual void Engine() = 0;
 
-    // Note: need to specify an explicicit default destructor
-    virtual ~Apple() = default;
+    virtual void Transmission() = 0;
+
+    virtual void Options() = 0;
+
+    // Explicit default destructor
+    virtual ~Subaru() = default;
 };
 
-class iPhone : public Apple {
+class Legacy : public Subaru {
 public:
-    void io() {
+    void Model()
+    {
+        cout << "Legacy\n";
+    }
+	
+    void Engine()
+    {
         cout << "iPhone: touchscreen\n";
     }
-};
 
-class iPad : public Apple {
-public:
-    void io() {
-        cout << "iPad: touchscreen & keyboard\n";
+    void Transmission()
+    {
+
+    }
+
+    void Options()
+    {
+
     }
 };
 
-class Mac : public Apple {
+class Impreza : public Subaru {
 public:
-    void io() {
-        cout << "Mac: keyboard\n";
+    void Model()
+    {
+        cout << "Impreza\n";
+    }
+	
+    void Engine()
+	{
+        cout << "SOHC 2.5L\n";
+    }
+
+	void Transmission()
+    {
+        cout << "5MT\n";
+    }
+
+	void Options()
+    {
+        cout << "Roof Rack\n";
     }
 };
 
+class Outback : public Subaru
+{
+public:
+    void Model()
+    {
+        cout << "Outback\n";
+    }
 
-// this is the FACTORY Method
-// it does need to know about the derived methods
-// but this allows us to abstract this from our 
-// "client" calling code in main()
-Apple* Apple::make_apple(string choice) {
-    if (choice == "iPhone")
-        return new iPhone;
-    else if (choice == "iPad")
-        return new iPad;
-    else
-        return new Mac;
+    void Engine()
+    {
+        cout << "iPhone: touchscreen\n";
+    }
+
+    void Transmission()
+    {
+
+    }
+
+    void Options()
+    {
+
+    }
+};
+
+class WRX : public Subaru {
+public:
+    void Model()
+    {
+        cout << "WRX\n";
+    }
+	
+    void Engine()
+    {
+        cout << "iPhone: touchscreen\n";
+    }
+
+    void Transmission()
+    {
+
+    }
+
+    void Options()
+    {
+
+    }
+};
+
+Subaru* Subaru::make_subaru(const char* model)
+{
+    int v = strcmp(model, "legacy");
+	
+    if (strcmp(model,"legacy") == 0)
+        return new Legacy;
+    else if (strcmp(model, "impreza") == 0)
+        return new Impreza;
+    else if (strcmp(model, "outback") == 0)
+        return new Outback;
+    else if (strcmp(model, "wrx") == 0)
+		return new WRX;
+
+    return nullptr;
 }
 
+const char* models[6] = { "legacy", "impreza", "outback", "wrx" };
 
-//  main() is considered the "client"
-// while in this example we could hard code the
-// derived classes since we are making a menu anyway
-// imagine having 
+void ToLower(char* svalue)
+{
+    for (unsigned int i = 0; i < strlen(svalue); i++)
+        svalue[i] = (char)tolower(svalue[i]);
+}
+
+char* CheckModel(char* model)
+{	
+    for (string m : models)
+    {
+        if (m == model)
+        {
+            return model;
+        }
+    }
+
+    return nullptr;
+}
 
 int main(int argc, char* argv[])
 {
-    for (int i=0;i<argc;i++)
+    if (argc < 2)
     {
-        cout << "Arg " << i << ":" << argv[i] << endl;
+        cerr << "Usage: " << argv[0] << " [Model] [Quantity]" << endl
+            << "Ex. Impreza 10 || Outback 5 || Wrx 11" << endl;
     }
+
+    vector<Subaru*> subarus;
 	
-    vector<Apple*> products;
-    string choice;
-    while (true) {
-        cout << "Enter Apple Product (Mac is default) or 0 if done: ";
-        cin >> choice;
-        if (choice == "0") break;
-        products.push_back(Apple::make_apple(choice));
-    }
-    for (int i = 0; i < products.size(); i++) {
-        products[i]->io();
-    }
-    for (int i = 0; i < products.size(); i++) {
-        delete products[i];
-    }
+	for (int idx=1; idx<argc;idx++)
+	{
+        ToLower(argv[idx]);
+		
+        Subaru* s = Subaru::make_subaru(argv[idx]);
+        if (s != nullptr)
+        {
+            subarus.push_back(s);
+            cout << "Subaru Model " << argv[idx] << " Constructed." << endl;
+        }
+        else
+            cout << "Skipping Invalid Model Name '" << argv[idx] << "'." << endl;	
+	}
+
+	for (unsigned int i=0; i<subarus.size();i++)
+	{
+        subarus[i]->Model();
+        subarus[i]->Engine();
+        subarus[i]->Transmission();
+        subarus[i]->Options();
+	}
 }
